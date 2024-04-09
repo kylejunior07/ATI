6ad4e191530dd6f7354156fb78c8c1c2ac47aa80
Ezekwe Osmond <148079073+kylejunior07@users.noreply.github.com>
2024-04-09 09:39:48 +0100

    Update Intro to programming ATI 2.py

--- Intro to programming ATI 2.py
+++ Intro to programming ATI 2.py
@@ -47,13 +47,13 @@ amount_entry.grid(column=1, row=0, sticky="ew", padx=10, pady=5)
 # Dropdown for FROM currency
 from_currency.set("USD") # Default value
 from_currency_dropdown = ttk.Combobox(root, textvariable=from_currency, state="readonly", font=('Arial', 12))
-from_currency_dropdown['values'] = ("USD", "EUR", "GBP", "JPY", "CAD", "INR", "NGN", "AUD", "CHF", "CNY", "SEK", "NZD", "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB", "BRL", "ZAR", "AED)
+from_currency_dropdown['values'] = ("USD", "EUR", "GBP", "JPY", "CAD", "INR", "NGN", "AUD", "CHF", "CNY", "SEK", "NZD", "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB", "BRL", "ZAR", "AED")
 from_currency_dropdown.grid(column=1, row=1, sticky="ew", padx=10, pady=5)
 
 # Dropdown for TO currency
 to_currency.set("EUR") # Default value
 to_currency_dropdown = ttk.Combobox(root, textvariable=to_currency, state="readonly", font=('Arial', 12))
-to_currency_dropdown['values'] = ("USD", "EUR", "GBP", "JPY", "CAD", "INR", "NGN", "AUD", "CHF", "CNY", "SEK", "NZD", "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB", "BRL", "ZAR")
+to_currency_dropdown['values'] = ("USD", "EUR", "GBP", "JPY", "CAD", "INR", "NGN", "AUD", "CHF", "CNY", "SEK", "NZD", "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB", "BRL", "ZAR" "AED")
 to_currency_dropdown.grid(column=1, row=2, sticky="ew", padx=10, pady=5)
 
 # Convert button
